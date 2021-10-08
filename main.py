d577fa91f7c21ab620c4d27b819046e74e4b9f22
David Rutkevich <74819392+DavidRutkevich@users.noreply.github.com>
2021-10-08 18:10:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ class Car(pygame.sprite.Sprite):
         self.alive = True
         self.radars = []
 
-
     def update(self, screen):
         self.radars.clear()
         self.rotate(screen)
@@ -38,13 +37,51 @@ class Car(pygame.sprite.Sprite):
         self.data()
         self.collision(screen)
 
-
-
     def drive(self):
-        self.rect.center += self.vel_vector*6
+        self.rect.center += self.vel_vector * 6
 
+    def collision(self, screen):
+        len = 40
+        collision_point_right = [
+            int(
+                self.rect.center[0]
+                + math.cos(math.radians(360 + 18 - self.angle)) * len
+            ),
+            int(
+                self.rect.center[1]
+                + math.sin(math.radians(360 + 18 - self.angle)) * len
+            ),
+        ]
+        collision_point_left = [
+            int(
+                self.rect.center[0]
+                + math.cos(math.radians(360 - 18 - self.angle)) * len
+            ),
+            int(
+                self.rect.center[1]
+                + math.sin(math.radians(360 - 18 - self.angle)) * len
+            ),
+        ]
 
+        if screen.get_at(collision_point_right) == pygame.Color(
+            2, 105, 31, 255
+        ) or screen.get_at(collision_point_left) == pygame.Color(2, 105, 31, 255):
+            self.alive = False
+            print("collision")
 
+        pygame.draw.circle(screen, (0, 255, 255, 0), collision_point_right, 4)
+        pygame.draw.circle(screen, (0, 255, 255, 0), collision_point_left, 4)
+
+    def rotate(self, screen):
+        if self.direction == 1:
+            self.angle -= self.rotation_vel
+            self.vel_vector.rotate_ip(self.rotation_vel)
+        if self.direction == -1:
+            self.angle += self.rotation_vel
+            self.vel_vector.rotate_ip(-self.rotation_vel)
+
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.1)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
     def data(self):
         radars = self.radars
@@ -53,38 +90,35 @@ class Car(pygame.sprite.Sprite):
             input[i] = int(radar[1])
         return input
 
-def remove(index):
-    cars.pop(index)
-    ge.pop(index)
-    nets.pop(index)
-
-
 
 def eval_genomes(genomes, config):
     global cars, ge, nets
-  
 
+    clock = pygame.time.Clock()
+
+    cars = []
+    ge = []
+    nets = []
+
+    for genome_id, genome in genomes:
+        cars.append(pygame.sprite.GroupSingle(Car()))
+        ge.append(genome)
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        genome.fitness = 0
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 
         SCREEN.blit(TRACK, (0, 0))
 
-        for i, car in enumerate(cars):
-            print(car.sprite.data())
-            output = nets[i].activate(car.sprite.data())
-            #print(output)
-            if output[0] > 0.7:
-                car.sprite.direction = -1
-            if output[1] > 0.7:
-                car.sprite.direction = 1
-            if output[0] <= 0.7 and output[1] <= 0.7:
-                car.sprite.direction = 0
-                
-                
         for car in cars:
             car.update(SCREEN)
             car.draw(SCREEN)
@@ -97,14 +131,31 @@ def eval_genomes(genomes, config):
             if not car.sprite.alive:
                 remove(i)
 
-
         clock.tick(30)
         pygame.display.update()
 
 
+def run(config_path):
+    global pop
+
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path,
+    )
+
+    pop = neat.Population(config)
+
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+
+    pop.run(eval_genomes, 50)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config.txt')
+    config_path = os.path.join(local_dir, "config.txt")
     run(config_path)
